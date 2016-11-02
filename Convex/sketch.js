b13d6e3438a7aaa7a61d11d4069e6e462808a6c7
capncapsize <daveng-2@student.ltu.se>
2016-11-02 09:22:17 +0100

    Add node by mouse

--- Convex/sketch.js
+++ Convex/sketch.js
@@ -1,12 +1,14 @@
 var p = [];
-var padding = 20;
+var padding = 0;
+
+var waitForRelease = true;
 
 function setup() {
 	createCanvas(255 + padding*2,255 + padding*2);
 
-	for (var i = 10; i >= 0; i--) {
+	/*for (var i = 10; i >= 0; i--) {
 		append(p,new Point(random(255), random(255)));
-	}
+	}*/
 	
 	
 }
@@ -20,29 +22,23 @@ function draw() {
 }
 
 
-function Point(x, y) {
+function Point(x, y, c) {
 	this.x = x + padding;
 	this.y = y + padding;
+	this.c = c;
 
 	this.show = function() {
-		fill(255);
+		fill(this.c);
 		rect(this.x, this.y, 10, 10);
 	}
 }
 
-function Snake() {
-	this.x = 0;
-	this.y = 0;
-	this.xv = 1;
-	this.yv = 0;
 
-	this.update = function() {
-		this.x = this.x + this.xv;
-		this.y = this.y + this.yv;
-	}
-
-	this.show = function() {
-		fill(255);
-		rect(this.x, this.y, 10, 10)
-	}
+function mergeSort(P) {
+	print(P);
 }
+
+function mouseReleased(){
+  	append(p,new Point(mouseX, mouseY, 255));
+  	mergeSort(p);
+  }
