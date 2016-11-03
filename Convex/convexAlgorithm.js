a76a9568bd22fe2dfcef6d7021f96849f5c97a8a
capncapsize <daveng-2@student.ltu.se>
2016-11-03 13:23:55 +0100

    Added mouse input and it WORKS :)

--- Convex/convexAlgorithm.js
+++ Convex/convexAlgorithm.js
@@ -12,41 +12,63 @@ function Point(x, y, c) {
 function PointSet(){
 	this.P = []
 
-	this.add =function(p){
+	this.add = function(p){
 		append(this.P, p);
-		this.P.sort(this.compareTo)
+	}
+
+	this.qsort = function(){
+		this.P.sort(this.compareTo);
 	}
 
 	this.show = function(){
 		for (var i = this.P.length - 1; i >= 0; i--) {
     		this.P[i].show();
   		}
-  		this.connect();
+  		this.connect(255);
 	}
 
-	this.connect = function(){
-	for(var i = this.P.length - 2; i>=0;i--){
-		stroke(255);
-		line(this.P[i].x, this.P[i].y, this.P[i+1].x, this.P[i+1].y);
+	this.connect = function(c){
+		stroke(c);
+		for(var i = this.P.length - 2; i>=0;i--){
+			line(this.P[i].x, this.P[i].y, this.P[i+1].x, this.P[i+1].y);
+		}
 	}
 
 	this.compareTo = function(a, b){
-		return a.x >= b.x;
-	}
-}
-	
+		return a.x - b.x;
+	}	
 }
 
 function convexHull(Set) {
 
-	for(var i = Set.P.length - 3; i>=0;i--){
-		if(0 > ccw(Set.P[i],Set.P[i+1],Set.P[i+2])){
-			stroke('red');
-			line(Set.P[i].x, Set.P[i].y, Set.P[i+2].x, Set.P[i+2].y)
+	var lower = new PointSet();
+	var upper = new PointSet();
+
+	for(var i = Set.P.length - 1; i>=0;i--){
+		while(upper.P.length >= 2 && ccw(upper.P[upper.P.length - 2], upper.P[upper.P.length - 1], Set.P[i]) <= 0){
+			upper.P.pop();
+			//stroke('red');
+			//line(Set.P[i].x, Set.P[i].y, Set.P[i+2].x, Set.P[i+2].y)
 		}
+		upper.add(Set.P[i]);
 	}
+
+	for (var i = 0; i < Set.P.length; i++) {
+		while(lower.P.length >= 2 && ccw(lower.P[lower.P.length - 2], lower.P[lower.P.length - 1], Set.P[i]) <= 0){
+			lower.P.pop();
+		}
+		lower.add(Set.P[i])
+	}
+	//reverse(lower.P)
+	//reverse(upper.P)
+
+	upper.connect('red');
+	lower.connect('red');
+
+	fill(255);
+
 }
 
-function ccw(p1, p2, p3){
-	return (p2.x - p1.x)*(p3.y - p1.y) - (p2.y - p1.y)*(p3.x - p1.x)
+function ccw(p1, p2, p3) {
+   return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x)
 }
--- Convex/sketch.js
+++ Convex/sketch.js
@@ -1,19 +1,25 @@
 var pSet;
-var padding = 0;
+var padding = 20;
 
 function setup() {
 	createCanvas(255 + padding*2,255 + padding*2);
 	pSet = new PointSet();
+
+
+//	for (var i = 5; i >= 0; i--) {
+//		pSet.add(new Point(round(random(255)), round(random(255)), 255));
+//	}
+//	pSet.qsort();
 }
 
 function draw() {
-  background(51);
-  pSet.show();
-
-  convexHull(pSet);
+  	background(51);
+  	pSet.show();
+	convexHull(pSet);
 }
 
 function mouseReleased(){
-  	pSet.add(new Point(mouseX, mouseY, 255));
+  	pSet.add(new Point(mouseX - padding, mouseY - padding, 255));
+  	pSet.qsort();
   }
 
